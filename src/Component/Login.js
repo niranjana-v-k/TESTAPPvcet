cf84901563824d94aa20f07ee8fe5a581a2ff825
niranjana <niranniranjana70@gmail.com>
2023-11-01 15:55:41 +0530

    first commit

---
+++ src/Component/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+     <h2>LOGIN</h2>
+    <div class="signin">
+   
+    </div>
+    </div>
+  )
+}
+
+export default Login
